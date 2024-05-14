f0a83822bb688332710a67bfdd7e64c7d88aedca
eryigithan16 <ygthnerygt1615@outlook.com>
2024-05-15 00:29:13 +0300

    dependencies updated and added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("io.realm.kotlin")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    //id("com.google.gms.google-services")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -63,4 +68,63 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation ("com.google.dagger:hilt-android:2.51.1")
+    kapt ("com.google.dagger:hilt-compiler:2.51.1")
+
+    // Compose Navigation
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
+
+    // Firebase
+    implementation ("com.google.firebase:firebase-auth-ktx:23.0.0")
+    implementation ("com.google.firebase:firebase-storage-ktx:21.0.0")
+
+    // Room components
+    implementation ("androidx.room:room-runtime:2.6.1")
+    ksp ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+
+    // Runtime Compose
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+
+    // Splash API
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+
+    // Mongo DB Realm
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation ("io.realm.kotlin:library-sync:1.11.0")
+
+    // Dagger Hilt
+    implementation ("com.google.dagger:hilt-android:2.51.1")
+    ksp ("com.google.dagger:hilt-compiler:2.51.1")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // Coil
+    implementation ("io.coil-kt:coil-compose:2.4.0")
+
+    // Pager - Accompanist [DEPRECATED]
+//    implementation "com.google.accompanist:accompanist-pager:0.27.0"
+
+    // Date-Time Picker
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:core:1.0.2")
+
+    // CALENDAR
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:calendar:1.0.2")
+
+    // CLOCK
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:clock:1.0.2")
+
+    // Message Bar Compose
+    implementation ("com.github.stevdza-san:MessageBarCompose:1.0.5")
+
+    // One-Tap Compose
+    implementation ("com.github.stevdza-san:OneTapCompose:1.0.7")
+
+    // Desugar JDK
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:2.0.4")
+
+
+}
+kapt {
+    correctErrorTypes = true
 }
