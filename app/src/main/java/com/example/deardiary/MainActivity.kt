da2411c9eaa05f4f7a4de7fe28770ea20ab98678
eryigithan16 <ygthnerygt1615@outlook.com>
2024-05-19 02:19:51 +0300

    splash screen created

--- app/src/main/java/com/example/deardiary/MainActivity.kt
+++ app/src/main/java/com/example/deardiary/MainActivity.kt
@@ -11,12 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.deardiary.ui.theme.DearDiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             DearDiaryTheme {
 
