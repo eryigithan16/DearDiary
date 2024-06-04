5e232c5ee74a7dc3e7c1ebf970e8bf9921b64805
eryigithan16 <ygthnerygt1615@outlook.com>
2024-06-04 18:01:42 +0300

    app-id and client_id values created

---
+++ app/src/main/java/com/example/deardiary/ui/theme/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.deardiary.ui.theme.util
+
+object Constants {
+    const val APP_ID = "deardearyapp-rrisjlr"
+    const val CLIENT_ID = "336858171318-koriakbahog5hpe5mcd7c3s56trcpt4q.apps.googleusercontent.com"
+}
