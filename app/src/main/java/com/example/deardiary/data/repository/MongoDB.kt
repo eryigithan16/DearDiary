dd68d5aa0e566f417a8eac322fe3bec5a2a73561
eryigithan16 <ygthnerygt1615@outlook.com>
2024-09-16 17:32:57 +0300

    filter kısmıyla alakalı bir bug vardı onu düzelttim.

--- app/src/main/java/com/example/deardiary/data/repository/MongoDB.kt
+++ app/src/main/java/com/example/deardiary/data/repository/MongoDB.kt
@@ -16,6 +16,8 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import org.mongodb.kbson.ObjectId
+import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
@@ -69,10 +71,20 @@ object MongoDB : MongoRepository {
         return if (user != null) {
             try {
                 realm.query<Diary>(
-                    "ownerId: $0 AND date < $1 AND date > $2",
+                    "ownerId == $0 AND date < $1 AND date > $2",
                     user.id,
-                    RealmInstant.from(zonedDateTime.plusDays(1).toInstant().epochSecond, 0),
-                    RealmInstant.from(zonedDateTime.minusDays(1).toInstant().epochSecond, 0)
+                    RealmInstant.from(
+                        LocalDateTime.of(
+                            zonedDateTime.toLocalDate().plusDays(1),
+                            LocalTime.MIDNIGHT
+                        ).toEpochSecond(zonedDateTime.offset), 0
+                    ),
+                    RealmInstant.from(
+                        LocalDateTime.of(
+                            zonedDateTime.toLocalDate(),
+                            LocalTime.MIDNIGHT
+                        ).toEpochSecond(zonedDateTime.offset), 0
+                    ),
                 ).asFlow().map { result ->
                     RequestState.Success(
                         data = result.list.groupBy {
